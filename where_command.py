4f63befe417b8816b7b45310f4504e0cdf2ee078
SodaCris <18463922396@163.com>
2022-01-08 10:26:22 +0800

    add path check

--- where_command.py
+++ where_command.py
@@ -6,11 +6,17 @@ if __name__ == "__main__":
         print("usage: where_command [command]")
         sys.exit(1)
     req_command = sys.argv[1]
-
+    found = False
     for path in paths:
+        if not os.path.exists(path):
+            print("cannot find: ", path)
+            continue
         files = os.listdir(path)
         for file in files:
             dot_pos = file.rfind(".")
             filename_no_ext = file[:dot_pos]
             if filename_no_ext == req_command:
                 print(os.path.join(path, file))
+                found = True
+    if not found:
+        print("command not found.")
